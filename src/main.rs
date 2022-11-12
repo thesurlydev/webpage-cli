0afd02ed099f9b0b9e1f17bcb2f5f6e244d058eb
Shane Witbeck <shane@digitalsanctum.com>
2022-11-12 07:57:15 -0800

    Save content to file in optional dir, Add tests

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,24 @@ extern crate serde;
 extern crate serde_derive;
 extern crate serde_json;
 
-use playwright::Playwright;
+use std::fs::File;
+use std::io::{Error, Write};
+use std::path::Path;
+use std::time::Duration;
 
 use clap::{Parser, Subcommand};
-use std::io::{Error, Write};
-use std::time::Duration;
+use playwright::Playwright;
 use webpage::{Webpage, WebpageOptions};
+
+use chrono::Local;
+use slugify::slugify;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let cli: Cli = Cli::parse();
     match &cli.command {
-        Command::Info { url, content } => {
-            let url: String = if url.starts_with("http://") || url.starts_with("https://") {
-                url.to_string()
-            } else {
-                format!("https://{}", url)
-            };
+        Command::Info { url, fetch, dir, .. } => {
+            let url = handle_scheme(url);
 
             let options = WebpageOptions {
                 allow_insecure: true,
@@ -29,18 +30,37 @@ async fn main() -> Result<(), Error> {
                 ..Default::default()
             };
 
-            let info: Webpage = Webpage::from_url(&*url, options).expect("Halp, could not fetch");
+            let info: Webpage =
+                Webpage::from_url(&*url, options).expect("Unable to interrogate URL");
             let info_json = serde_json::to_string_pretty(&info);
 
-            if *content {
+            if *fetch {
                 let fetch_result = fetch_content(&url)
                     .await
                     .expect("Error fetching content for ${url}");
-                // save string to file
-                let mut file =
-                    std::fs::File::create("content.html").expect("Unable to create file");
-                file.write_all(fetch_result.as_bytes())
-                    .expect("Unable to write data");
+                let url_slug = slugify!(&url, stop_words = "https,http,www");
+                let timestamp = Local::now().format("_%Y-%m-%d-%H%M%S");
+                let file_name = format!("{url_slug}{timestamp}.html");
+
+                let maybe_file = if dir.is_some() {
+                    let dir_str = dir.as_ref().unwrap();
+                    let parent_dir = Path::new(dir_str);
+                    if !parent_dir.exists() {
+                        std::fs::create_dir_all(parent_dir).expect("Error creating directory");
+                    }
+                    let path = parent_dir.join(file_name);
+                    File::create(path)
+                } else {
+                    File::create(file_name)
+                };
+
+                match maybe_file {
+                    Ok(mut file) => {
+                        file.write_all(fetch_result.as_bytes())
+                            .expect("Unable to write data");
+                    }
+                    Err(err) => eprintln!("Error creating file: {}", err),
+                }
             }
 
             match info_json {
@@ -52,17 +72,30 @@ async fn main() -> Result<(), Error> {
     Ok(())
 }
 
+/// Ensure url contains scheme
+fn handle_scheme(url: &String) -> String {
+    return if url.starts_with("http://") || url.starts_with("https://") {
+        url.to_string()
+    } else {
+        format!("https://{}", url)
+    };
+}
+
 #[derive(Subcommand)]
 enum Command {
     /// Prints information about a webpage
     #[clap(alias = "i")]
     Info {
-        /// The URL to interrogate
-        url: String,
-
         /// Fetch content via playwright
         #[arg(short, long, default_value = "false")]
-        content: bool,
+        fetch: bool,
+
+        /// Directory to save content
+        #[arg(short, long, default_value = "./")]
+        dir: Option<String>,
+
+        /// The URL to interrogate
+        url: String,
     },
 }
 
@@ -83,4 +116,29 @@ async fn fetch_content(url: &str) -> Result<String, std::sync::Arc<playwright::E
     let page = context.new_page().await?;
     page.goto_builder(url).goto().await?;
     page.content().await
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn handle_scheme_no_scheme() {
+        let url = "example.com".to_string();
+        let result = handle_scheme(&url);
+        assert_eq!(result, "https://example.com");
+    }
+
+    #[test]
+    fn handle_scheme_http() {
+        let url = "http://example.com".to_string();
+        let result = handle_scheme(&url);
+        assert_eq!(result, url);
+    }
+
+    #[test]
+    fn handle_scheme_https() {
+        let url = "https://example.com".to_string();
+        let result = handle_scheme(&url);
+        assert_eq!(result, url);
+    }
 }
